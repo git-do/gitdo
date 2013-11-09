a9d0830465aa0c4a6e224e4a34748ee9af177a42
Alex Mesarosh <alex.mesarosh@meltmedia.com>
2013-11-09 10:18:55 -0700

    webhook debugging

--- routes/github/hooks.js
+++ routes/github/hooks.js
@@ -77,9 +77,9 @@ exports.removeHookRoute = function (req, res) {
 exports.webHook = function (req, res) {
   var payload = req.body.payload,
       payloadInfo = {};
-  console.log(payload);
+
   payloadInfo.repo = payload.repository.name;
-  payloadInfo.user = payload.repository.owner.name;
+  // payloadInfo.user = payload.repository.owner.name;
   payloadInfo.ref = payload.ref;
   payloadInfo.changedFiles = [];
 
@@ -96,7 +96,7 @@ exports.webHook = function (req, res) {
 
     // todo: give these files to the parser
     var parser = new Parser();
-    files.getFile('eca89c2aa0c1e8fd867efb6da061792400c6efdd', payloadInfo.repo, payloadInfo.user, payloadInfo.changedFiles[0], payloadInfo.ref, function (err, data) {
+    files.getFile('eca89c2aa0c1e8fd867efb6da061792400c6efdd', payloadInfo.repo, 'gitdo', payloadInfo.changedFiles[0], payloadInfo.ref, function (err, data) {
       parser.parseCode(data);
     });
   });
