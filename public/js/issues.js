f4fa792e9718c6e4a2e8da0ebbbbab72f454cca9
Michael Wolf <michael.wolf@meltmedia.com>
2013-11-10 08:19:13 -0700

    Implemented issue tab functionality

--- public/js/issues.js
+++ public/js/issues.js
@@ -1,10 +1,35 @@
-$(function () {
+(function($){
 
-  $('.issue-list li').on("click", function(ev) {
-    ev.preventDefault();
-    var _this = this;
-    $(this).find(".issue-details").slideToggle();
-    $(this).toggleClass('open');
+  $(function () {
+
+    // Issue List Item Slide Toggle
+    $('.issue-list .list-item-header').on("click", function(ev) {
+      ev.preventDefault();
+      var _this = this;
+      $(this).closest("li").find(".issue-details").slideToggle();
+      $(this).closest("li").toggleClass('open');
+    });
+
+    // Issue Tabs
+    $("[data-tab-content]").on("click", function (ev) {
+      ev.preventDefault();
+
+      // Get Selected tab-content
+      var targetSection = $(this).data("tab-content");
+      
+      // Remove all active classes
+      $("[data-tab-content]").removeClass("active");
+
+      // Add active class to target
+      $("[data-tab-content=" + targetSection + "]").each(function () {
+        $(this).addClass("active");
+      });
+
+    });
+
+    // Select open issues first
+    $("[data-tab-content=open-issues]").trigger("click");
+
   });
 
-});
+}(jQuery));
